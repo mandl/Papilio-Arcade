754ce5bcf5b25c6178f42a2f3d4a8d13c9262aeb
Mandl <mandl@users.noreply.github.com>
2015-05-14 10:56:56 +0200

    add support for ppro and configs

---
+++ A2601/A2601/src/config_rom.vhd
@@ -0,0 +1,97 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity config_rom is
+  port (
+    CLK         : in    std_logic;
+	
+    DATA        : out   std_logic_vector(3 downto 0)
+    );
+end;
+
+architecture Behavioral of config_rom is
+
+function romgen_str2bv (str : string) return bit_vector is
+    variable result : bit_vector (str'length*4-1 downto 0);
+  begin
+    for i in 0 to str'length-1 loop
+      case str(str'high-i) is
+        when '0'       => result(i*4+3 downto i*4) := x"0";
+        when '1'       => result(i*4+3 downto i*4) := x"1";
+        when '2'       => result(i*4+3 downto i*4) := x"2";
+        when '3'       => result(i*4+3 downto i*4) := x"3";
+        when '4'       => result(i*4+3 downto i*4) := x"4";
+        when '5'       => result(i*4+3 downto i*4) := x"5";
+        when '6'       => result(i*4+3 downto i*4) := x"6";
+        when '7'       => result(i*4+3 downto i*4) := x"7";
+        when '8'       => result(i*4+3 downto i*4) := x"8";
+        when '9'       => result(i*4+3 downto i*4) := x"9";
+        when 'A'       => result(i*4+3 downto i*4) := x"A";
+        when 'B'       => result(i*4+3 downto i*4) := x"B";
+        when 'C'       => result(i*4+3 downto i*4) := x"C";
+        when 'D'       => result(i*4+3 downto i*4) := x"D";
+        when 'E'       => result(i*4+3 downto i*4) := x"E";
+        when 'F'       => result(i*4+3 downto i*4) := x"F";
+        when others    => null;
+      end case;
+    end loop;
+    return result;
+  end romgen_str2bv;
+  
+attribute INIT_00 : string;
+
+component RAMB16_S4
+    --pragma translate_off
+    generic (
+      INIT_00 : bit_vector (255 downto 0) := x"0000000000000000000000000000000000000000000000000000000000000000"
+ );
+    --pragma translate_on
+    port (
+      DO    : out std_logic_vector (3 downto 0);
+      ADDR  : in  std_logic_vector (11 downto 0);
+      CLK   : in  std_logic;
+      DI    : in  std_logic_vector (3 downto 0);
+      EN    : in  std_logic;
+      SSR   : in  std_logic;
+      WE    : in  std_logic 
+      );
+end component;
+
+signal rom_addr : std_logic_vector(13 downto 0);
+
+begin
+
+ 
+
+ rom0 : if true generate
+    attribute INIT_00 of inst : label is "000000000000000000000000000000000000000000000000000000000000000";
+begin
+inst : RAMB16_S4
+      --pragma translate_off
+      generic map (
+        INIT_00 => romgen_str2bv(inst'INIT_00)
+		      )
+      --pragma translate_on
+      port map (
+        DO   => DATA(3 downto 0),
+        ADDR => (others => '0'),
+        CLK  => CLK,
+        DI   => "0000",
+        EN   => '1',
+        SSR  => '0',
+        WE   => '0'
+        );
+  end generate;
+	
+
+
+
+end Behavioral;
+
